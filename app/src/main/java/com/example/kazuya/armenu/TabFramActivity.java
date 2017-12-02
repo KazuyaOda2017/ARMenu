d57928e9f9f00523ae17e125756727a5c61d770c
kazuya <aqua.timez.gto@gmail.com>
2017-12-02 11:06:13 +0900

    tab追加

---
+++ app/src/main/java/com/example/kazuya/armenu/TabFramActivity.java
@@ -0,0 +1,19 @@
+package com.example.kazuya.armenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by aquat on 2017/12/02.
+ */
+
+public class TabFramActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frame);
+    }
+
+
+}
